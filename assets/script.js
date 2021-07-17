41a6e360b2a56546c6bffdd5fd60df151df696b9
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-07-16 18:45:14 -0700

    javascript run questions

--- assets/script.js
+++ assets/script.js
@@ -2,56 +2,236 @@
 
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
-var question = document.getElementById("question");
-var choiceA = document.getElementById("a");
-var choiceB = document.getElementById("b");
-var choiceC = document.getElementById("c");
-var choiceD = document.getElementById("d");
+var questionDiv = document.getElementById("question");
+var choiceA = document.getElementById("choiceAA");
+var choiceB = document.getElementById("choiceBB");
+var choiceC = document.getElementById("choiceCC");
+var choiceD = document.getElementById("choiceDD");
 var counter = document.getElementById("counter")
 var timeGuage = document.getElementById("timeGuage");
 var answer = document.getElementById("answer");
 var scoreDiv = document.getElementById("scoreContainer");
+var score = 0;
 
 //questions
 
-var question = [
+var questions = [
    { question : "what does HTML stand for?",
-    choiceA : "Happy Times Mean Laughter",
-    choiceB : "His Temper Might Linger",
-    choiceC : "Hypertext Markup Language",
-    choiceD : "trick question, it doesn't mean anything",
-    correct : "c"
+    choiceAA : "Happy Times Mean Laughter",
+    choiceBB : "His Temper Might Linger",
+    choiceCC: "Hypertext Markup Language",
+    choiceDD : "trick question, it doesn't mean anything",
+    correct : "choiceCC"
 },
 
     { question : "what does CSS stand for?",
-    choiceA : "Cascading Style Sheets",
-    choiceB : "Cant Stop Smiling",
-    choiceC : "Card Style's Sharp",
-    choiceD : "Common Stunt Starts",
-    correct : "a"
+    choiceAA : "Cascading Style Sheets",
+    choiceBB : "Cant Stop Smiling",
+    choiceCC : "Card Style's Sharp",
+    choiceDD : "Common Stunt Starts",
+    correct : "choiceAA"
 },
 
  { question : "what does JS stand for?",
-    choiceA : "Jorts",
-    choiceB : "Justice Served",
-    choiceC : "JavaScript",
-    choiceD : "Just Sayin'",
-    correct : "c"
+    choiceAA : "Jorts",
+    choiceBB : "Justice Served",
+    choiceCC : "JavaScript",
+    choiceDD : "Just Sayin'",
+    correct : "choiceCC"
 },
 
     { question : "The condition in an if / else statement is enclosed with ___________.",
-    choiceA : "Quotes",
-    choiceB : "Curly brackets",
-    choiceC : "Parenthesis",
-    choiceD : "Square brackets",
-    correct : "c"
+    choiceAA : "Quotes",
+    choiceBB : "Curly brackets",
+    choiceCC : "Parenthesis",
+    choiceDD : "Square brackets",
+    correct : "choiceCC"
 },
 
     { question : "Arrays in Javascript can be used to store _____________.",
-    choiceA : "Numbers and strings.",
-    choiceB : "Other arrays",
-    choiceC : "Booleans",
-    choiceD : "All of the above.",
-    correct : "d"
+    choiceAA : "Numbers and strings.",
+    choiceBB : "Other arrays",
+    choiceCC : "Booleans",
+    choiceDD : "All of the above.",
+    correct : "choiceDD"
 }
 ];
+
+
+var lastQuestion = question.length - 1;
+var runningQuestion = 0;
+
+function renderQuestion(){
+   if(questions[runningQuestion]){ let q = questions[runningQuestion];
+
+    questionDiv.innerHTML = "<p>" + q.question + "</p>";
+
+    choiceA.innerHTML = q.choiceAA;
+    choiceB.innerHTML = q.choiceBB;
+    choiceC.innerHTML = q.choiceCC;
+    choiceD.innerHTML = q.choiceDD;}
+    else{
+        //go to high score page
+    }
+};
+
+choiceA.addEventListener("click", function(event){
+    let q = questions[runningQuestion]; 
+    console.log(event.target.id);
+    console.log(q.correct); 
+    if (event.target.id == q.correct) {
+        score++;
+        console.log(score);
+        runningQuestion++;
+        renderQuestion();
+    }
+    else{
+        runningQuestion++;
+        renderQuestion();
+    }
+});
+choiceB.addEventListener("click", function(event){
+    let q = questions[runningQuestion]; 
+    console.log(event.target.id);
+    console.log(q.correct); 
+    if (event.target.id == q.correct) {
+        score++;
+        console.log(score);
+        runningQuestion++;
+        renderQuestion();
+    }
+    else{
+        runningQuestion++;
+        renderQuestion();
+    }
+});
+choiceC.addEventListener("click", function(event){
+    let q = questions[runningQuestion]; 
+    console.log(event.target.id);
+    console.log(q.correct); 
+    if (event.target.id == q.correct) {
+        score++;
+        console.log(score);
+        runningQuestion++;
+        renderQuestion();
+    }
+    else{
+        runningQuestion++;
+        renderQuestion();
+    }
+});
+choiceD.addEventListener("click", function(event){
+    let q = questions[runningQuestion]; 
+    console.log(event.target.id);
+    console.log(q.correct); 
+    if (event.target.id == q.correct) {
+        score++;
+        console.log(score);
+        runningQuestion++;
+        renderQuestion();
+    }
+    else{
+        runningQuestion++;
+        renderQuestion();
+    }
+})
+
+start.style.display = "none";
+renderQuestion();
+quiz.style.display = "block";
+
+//render progress
+function renderProgress (){
+    for(let qIndex = 0; qIndex <= lastQuestion;
+        qIndex++){
+            ProgressEvent.innerHTML += "<div class ='prog' id=" + qIndex + "></div>";
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// renderQuestion();
+
+// runningQuestionIndex++;
+// renderQuestion();
+
+// const questionTime = 10;
+// // const guageWidth = 150;
+// let count = 0;
+// const guageProgressUnit = gugageWidth/ questionTime;
+
+// let TIMER = setInterval(counterRender, 1000);
+
+
+// function counterRender(){
+//     if(count <= questionTime){
+//         counter.innerHTML = count;
+//         timeGuage.style.width = guageProgressUnit * count + "px";
+//         count++;
+//     }
+//     else{
+//         count = 0;
+//         answerIsWrong();
+//         if(runningQuestionIndex < lastQuestionIndex){
+//             runningQuestionIndex++;
+//             questionRender();
+//         }
+//         else{
+//             clearInterval(TIMER);
+//             scoreRender();
+//         }
+//     }
+// };
+
+// function checkAnswer(answer){
+//     if(question[runningQuestionIndex].correct == answer){
+//         score++;
+//         answerIsCorrect(); 
+//     }
+//     else{
+//         answerIsWrong();
+//     }
+//     if(runningQuestionIndex < lastQuestionIndex){
+//         count = 0;
+//         runningQuestionIndex++;
+//         questionRender();
+//     }
+//     else{
+//         clearInterval(TIMER);
+
+//         scoreRender();
+//     }
+// };
+
+// const start = document.getElementById("start");
+
+// start.addEventListener("click",startQuiz);
+
+// let TIMER;
+
+// function startQuiz(){
+//     start.style.display = "none";
+//     counterRender();
+//     TIMER = setInterval(counterRender,1000);
+//     progressRender();
+//     questionRender();
+//     quiz.style.display = "block";
+// };
+
+// function scoreRender(){
+//     ServiceWorkerContainer.style.display = "block";
+//     let scorePerCent = Math.round(100 * score/ questions.length);
+
+// }
